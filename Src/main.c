6892cae7cfe0c909f92e70752f59a356a9594286
exleyt <trevynexley@gmail.com>
2024-12-16 04:40:48 -0800

    Receiving data and printing

--- Src/main.c
+++ Src/main.c
@@ -22,19 +22,29 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "string.h"
+#include "stdlib.h"
 #include "stdio.h"
 #include "nrf24.h"
+#include "stdbool.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-
+typedef enum transmit_size
+{
+	SIZE_REQUIRED_DATA_ONLY = 5,
+	SIZE_SPECTRA_DATA_ONLY = 9,
+	SIZE_VALIDATION_DATA = 10, // required + 5 validation parameters
+	SIZE_SPECTRA_DATA = 13, // required data + other 8 spectra data
+	SIZE_ALL_DATA = 18,
+} transmit_size;
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 #define MAXIMUM_PRINT_TIMEOUT (uint32_t) 100 // Maximum time-out to wait for any print ACK
 #define MAX_PRINT_LENGTH (uint32_t) 100 // Maximum lpuart1 serial data buffer length
+#define MAX_PAYLOAD_PRINT_TIMEOUT (uint32_t) 60000 // Maximum time-out to wait without receiving data before printing
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -48,6 +58,16 @@ SPI_HandleTypeDef hspi1;
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
+const uint8_t NRF24_ADDR[] = { 'B', 'o', 'y', '0', '1' }; // The TX address
+const uint8_t NRF24_ADDR_SIZE = 5; // Must in the range 3 - 5
+const uint8_t NRF24_CHANNEL = 1; // Transmits on the frequency of 2400Mhz + this
+
+const uint8_t NRF24_HEADER_CHECK = 0b11111111;
+const uint8_t NRF24_BODY_CHECK = 0b0;
+const uint8_t NRF24_CHECK_BIT = 3;
+
+const char* data_names[SIZE_ALL_DATA] = {"Rhq\0", "Phih\0", "gamma2\0", "gamma3\0", "lambda\0", "r1\0", "a1\0", "r2\0", "a2\0", "C11m\0", "C22m\0", "C33m\0", "C23m\0", "Q12m\0", "C12m\0", "Q13m\0", "C13m\0", "Q23m\0"};
+float** data;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -57,7 +77,7 @@ static void MX_USART2_UART_Init(void);
 static void MX_SPI1_Init(void);
 /* USER CODE BEGIN PFP */
 void serial_print(const char* buffer);
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin);
+void print_data(uint8_t data_size, uint16_t sample_size);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -106,22 +126,12 @@ int main(void)
 	}
 	serial_print("Initialized nRF24.\r\n");
 
-	/*
-			    float f = 1;
-				float x = 3.14159f;
-				uint32_t *bits = (uint32_t *) &x;
-
-				printf("Float: %f\n", x);
-				printf("Bits: %08x\n", *bits);
-			 */
-
-	uint8_t ADDR[] = { 'n', 'R', 'F', '2', '4' }; // the address for RX pipe
-	nRF24_SetRFChannel(24); // set RF channel to 2490MHz
+	nRF24_SetRFChannel(NRF24_CHANNEL); // set RF channel to 2424MHz
 	nRF24_SetDataRate(nRF24_DR_1Mbps); // 2Mbit/s data rate
 	nRF24_SetCRCScheme(nRF24_CRC_2byte); // 1-byte CRC scheme
-	nRF24_SetAddrWidth(5); // address width is 5 bytes
-	nRF24_SetAddr(nRF24_PIPE1, ADDR); // program pipe address
-	nRF24_SetRXPipe(nRF24_PIPE1, nRF24_AA_ON, 3); // enable RX pipe#1 with Auto-ACK: enabled, payload length: 10 bytes
+	nRF24_SetAddrWidth(NRF24_ADDR_SIZE); // address width is 5 bytes
+	nRF24_SetAddr(nRF24_PIPE1, NRF24_ADDR); // program pipe address
+	nRF24_SetRXPipe(nRF24_PIPE1, nRF24_AA_ON, 32); // enable RX pipe#1 with Auto-ACK: enabled, payload length: 10 bytes
 	nRF24_SetTXPower(nRF24_TXPWR_0dBm); // configure TX power for Auto-ACK, good choice - same power level as on transmitter
 	nRF24_SetOperationalMode(nRF24_MODE_RX); // switch transceiver to the RX mode
 	nRF24_SetPowerMode(nRF24_PWR_UP); // wake-up transceiver (in case if it sleeping)
@@ -132,6 +142,16 @@ int main(void)
 	uint8_t nRF24_payload[32]; // buffer for payload
 	uint8_t payload_length; // variable to store a length of received payload
 
+	uint16_t sample_size;
+	transmit_size data_size;
+
+	uint32_t data_col_start, data_col_end, data_proc_end, transmit_start_time;
+	bool transmitting = false;
+
+	//uint32_t *bits = (uint32_t *) &(data_outf[i][n + j]);
+	//sprintf(str, "%hu %hu %hu\r\n", nRF24_payload[0], nRF24_payload[1], nRF24_payload[2]);
+	//serial_print(str);
+	// NRF24_HEADER_CHECK
 	nRF24_ClearIRQFlags(); // clear any pending IRQ bits
 	nRF24_CE_H; // start receiving
 	while (1) {
@@ -148,9 +168,65 @@ int main(void)
     		// Clear all pending IRQ flags
 			nRF24_ClearIRQFlags();
 
-			sprintf(str, "%hu %hu %hu\r\n", nRF24_payload[0], nRF24_payload[1], nRF24_payload[2]);
-			serial_print(str);
+			if (nRF24_payload[NRF24_CHECK_BIT] == NRF24_BODY_CHECK && transmitting) // 3
+			{
+				uint8_t payload_size = nRF24_payload[0] & 0b00000111;
+				uint16_t start_ind;
+				memcpy(&start_ind, nRF24_payload + 1, sizeof(uint16_t)); // 1, 2
+
+				if (start_ind + payload_size > sample_size) // Ignore packet with oob data
+					continue;
+
+				uint8_t data_ind = nRF24_payload[0] & 0b11111000 >> 3;
+				memcpy(data[data_ind] + start_ind, nRF24_payload + sizeof(uint32_t), payload_size); // 4 -> 31
+
+				if (start_ind + payload_size == sample_size)
+				{
+					sprintf(str, "Transmition %d%% complete.", (uint16_t) (data_ind + 1) * 100 / data_size);
+					if (data_ind + 1 == data_size) // If transmission is done
+					{
+						print_data(data_size, sample_size);
+						transmitting = false;
+					}
+				}
+
+			} else if (nRF24_payload[NRF24_CHECK_BIT] == NRF24_HEADER_CHECK) // 3
+			{
+				if (transmitting)
+				{
+					print_data(data_size, sample_size);
+					transmitting = false;
+				}
+
+				data_size = nRF24_payload[0]; // 0
+				if (!(data_size == SIZE_REQUIRED_DATA_ONLY || data_size == SIZE_SPECTRA_DATA_ONLY
+					|| data_size == SIZE_VALIDATION_DATA || data_size == SIZE_SPECTRA_DATA || data_size == SIZE_ALL_DATA))
+					continue; // Ingore if invalid size in header
+
+				memcpy(&sample_size, nRF24_payload + sizeof(uint8_t), sizeof(uint16_t)); // 1, 2
+				memcpy(&data_col_start, nRF24_payload + sizeof(uint32_t), sizeof(uint32_t)); // 4, 5, 6, 7
+				memcpy(&data_col_end, nRF24_payload + sizeof(uint32_t) * 2, sizeof(uint32_t)); // 8, 9, 10, 11
+				memcpy(&data_proc_end, nRF24_payload + sizeof(uint32_t) * 3, sizeof(uint32_t)); // 12, 13, 14, 15
+				// 16 -> 31 bits unused in header
+
+				data = (float**) malloc(sizeof(float) * data_size);
+				for (int i = 0; i < data_size; i++)
+					data[i] = (float*) calloc(sizeof(float), sample_size);
+
+				serial_print("Receiving data from Bouy01.\r\n");
+				sprintf(str, "Receiving %u arrays of length %u.\r\n", data_size, sample_size);
+				serial_print(str);
+
+				transmit_start_time = HAL_GetTick();
+				transmitting = true;
+			}
+			// If checkbit is wrong we discard the packet
+    	} else if (transmitting && HAL_GetTick() - transmit_start_time >= MAX_PAYLOAD_PRINT_TIMEOUT)
+    	{
+    		print_data(data_size, sample_size);
+    		transmitting = false;
     	}
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -340,6 +416,30 @@ static void MX_GPIO_Init(void)
 void serial_print(const char* buffer)
 {
 	HAL_UART_Transmit(&huart2, (uint8_t*) buffer, strlen(buffer), MAXIMUM_PRINT_TIMEOUT);
+}
+
+void print_data(uint8_t data_size, uint16_t sample_size)
+{
+	uint8_t data_name_offset = (data_size == SIZE_VALIDATION_DATA || data_size == SIZE_ALL_DATA)
+		? 0 : SIZE_VALIDATION_DATA - SIZE_REQUIRED_DATA_ONLY;
+
+	for (int i = 0; i < data_size; i++)
+	{
+		sprintf(str, "%s, ", data_names[data_name_offset + i]);
+		serial_print(str);
+	}
+	serial_print("\r\n");
+
+	for (int i = 0; i < sample_size; i++)
+	{
+		for (int j = 0; j < data_size; j++)
+		{
+			sprintf(str, "%f, ", data[j][i]);
+			serial_print(str);
+		}
+		serial_print("\r\n");
+	}
+
 }
 /* USER CODE END 4 */
 
